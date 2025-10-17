886e815a4b895926742295cf8627e51fcd39db8e
John Carlos Soldevilla <soldevillajohncarlos@gmail.com>
2025-10-17 23:15:00 +0800

    Deployment to GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,11 @@ import type { NextConfig } from "next";
 import withFlowbiteReact from "flowbite-react/plugin/nextjs";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export', // Enable static exports
+  basePath: '/getfeeds', // Replace with your repo name
+  images: {
+    unoptimized: true, // Required for static export
+  },
 };
 
 export default withFlowbiteReact(nextConfig);
